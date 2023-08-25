5fc7e550a42b204452c6c24f4216e02ccab2cfa4
MajorDown <romain.fouillaron@gmx.fr>
2023-08-25 21:37:17 +0200

    poursuite bio

--- src/app/bio/page.js
+++ src/app/bio/page.js
@@ -5,8 +5,11 @@ import SiteData from "../../data/siteData";
 const Bio = () => {
   return (
     <section id="bioSection">
-      <BioArticle src={SiteData.bio.intro} imagePosition="left" />
+      <BioArticle src={SiteData.bio.intro} imagePosition="Left" />
       <BioArticle src={SiteData.bio.Emile} imagePosition="right" />
+      <BioArticle src={SiteData.bio.Philippe} imagePosition="left" />
+      <BioArticle src={SiteData.bio.Romain} imagePosition="right" />
+      <BioArticle src={SiteData.bio.Jean_Paul} imagePosition="left" />
     </section>
   );
 };
--- src/data/siteData.js
+++ src/data/siteData.js
@@ -14,6 +14,27 @@ const SiteData = {
       vignette: "lolilol",
       url: "/accueil.jpg",
     },
+    Philippe: {
+      title: "lolilol",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores et commodi hic? Incidunt, sunt quis?",
+      vignette: "lolilol",
+      url: "/accueil.jpg",
+    },
+    Romain: {
+      title: "lolilol",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores et commodi hic? Incidunt, sunt quis?",
+      vignette: "lolilol",
+      url: "/accueil.jpg",
+    },
+    Jean_Paul: {
+      title: "lolilol",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores et commodi hic? Incidunt, sunt quis?",
+      vignette: "lolilol",
+      url: "/accueil.jpg",
+    },
   },
 };
 
